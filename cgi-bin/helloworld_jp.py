d7d676a7ecf57a5f7341e93d06ebe856c518487a
Bukio-chan <bukky817@outlook.jp>
2020-11-12 21:05:34 +0900

    make it better

---
+++ cgi-bin/helloworld_jp.py
@@ -0,0 +1,9 @@
+import cgitb
+cgitb.enable()
+
+print ("Content-Type: text/html\n");
+print ("こんにちは　日本語！");
+
+print("<html><body>")
+print("img src="../image/USJ_map.png">")
+print("</body><html>")
