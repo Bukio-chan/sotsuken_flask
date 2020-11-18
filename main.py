1d7372578cc5924f9473eb6d406954f69d4f6520
Bukio-chan <bukky817@outlook.jp>
2020-11-18 21:57:27 +0900

    キャッシュクリア

--- main.py
+++ main.py
@@ -140,5 +140,15 @@ def result():
                            distance=distance_result, order=order_result, img_url=img_url)
 
 
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+
+    return r
+
+
 if __name__ == "__main__":
     app.run(debug=True)
